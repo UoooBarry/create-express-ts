c535050dd11a3aa4c055d0365e9a6df090ba335b
UoooBarry <s3719368@student.rmit.edu.au>
2020-10-27 15:53:36 +1100

    added path argument

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+const shell = require('shelljs');
 const args = process.argv.slice(2);
 
-console.log(args);
+argument = args[0];
+
+
+if(argument){
+    if(argument.startsWith('.')){
+        const path  = argument.split('/')[1];
+        if(path){
+            console.log('create-express-ts in /' + path);
+            cloneEmptyProject(`./${path}`);
+        }else{
+            console.log('create-express-ts in current folder');
+            cloneEmptyProject('.');
+        }
+    }
+    else 
+        printHelp();
+}
+
+function cloneEmptyProject(path){
+    shell.exec(`git clone https://github.com/UoooBarry/empty-express-ts ${path}`);
+    console.log("🌈 🌈 🌈 Installing dependencies...");
+    installDeps(path);
+    console.log('🍺 🍺 🍺! Enjoy!');
+}
+
+function installDeps(path){
+    const installCommand = 'npm install';
+    if(path === '.')
+        shell.exec(installCommand);
+    else
+        shell.exec(`cd ${path} && ${installCommand}`);
+}
+
+function printHelp(){
+    console.log('arguments:');
+    console.log("1.path: input . = current folder, ./folder = folder inside current folder")
+}
